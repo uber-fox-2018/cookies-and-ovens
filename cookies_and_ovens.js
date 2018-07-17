74d3e121c6641bbbf933ae281ad5feb559ab6493
Andre Sudi Yudatama <andresudi@DDCBU-elee.localdomain>
2018-07-17 20:22:31 +0700

    cookies and ovens

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -8,3 +8,50 @@
 //
 //
 // Your code here
+
+class Cake {
+    constructor(num, name) {
+        this.bakedTime = num
+        this.name = name
+    }
+
+    cook(time) {
+        for (let i = 0; i < time; i+=5) {
+            if (i < this.bakedTime - 5) {
+                console.log(`${this.name}, menit ke ${i}: mentah`)
+            } else if (i === this.bakedTime - 5) {
+                console.log(`${this.name}, menit ke ${i}: hampir matang`)
+            } else if (i === this.bakedTime) {
+                console.log(`${this.name}, menit ke ${i}: matang`)
+            } else {
+                console.log(`${this.name}, menit ke ${i}: hangus`)
+            }
+        }
+    }
+}
+
+class Cokelat extends Cake {
+    constructor() {
+        super(20, 'Kue cokelat')
+    }
+}
+
+class Kacang extends Cake {
+    constructor() {
+        super(30, 'Kue Kacang')
+    }
+}
+
+class Keju extends Cake {
+    constructor() {
+        super(35, 'Kue Keju')
+    }
+}
+
+var cokelat = new Cokelat()
+var kacang = new Kacang()
+var keju = new Keju()
+
+cokelat.cook(20)
+
+
