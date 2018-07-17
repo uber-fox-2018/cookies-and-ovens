ea732050a7426359d1bd00fb8ac657f7741f45b0
Odelia <odeliasusanto@gmail.com>
2018-07-17 21:13:16 +0700

    baking done

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -1,10 +1,62 @@
-// Answer These Questions:
-//
-// - What are essential classes?
-// - What attributes will each class have?
-// - What interface will each class provide?
-// - How will the classes interact with each other?
-// - Which classes will inherit from others, if any?
-//
-//
-// Your code here
+class Cake {
+    constructor(name, bakingTime) {
+        this.name = name;
+        this.bakingTime = bakingTime;
+    }
+
+}
+
+class ChocCake extends Cake {
+    constructor(name, bakingTime) {
+        super('Chocolatey', 20);
+    }
+}
+
+class PeanutCake extends Cake {
+    constructor(name, bakingTime) {
+        super('Go Nuts', 30);
+    }
+}
+
+class CheeseCake extends Cake {
+    constructor(name, bakingTime) {
+        super('Cheesus', 35);
+    }
+}
+
+class Oven {
+    constructor(cakeList) {
+        this.cakeList = cakeList;
+    }
+
+    bake(time) {
+        for (let i = 0; i < this.cakeList.length; i++) {
+            for (let j = 5; j < time; j+=5) {
+                if (j === this.cakeList[i].bakingTime) {
+                    console.log(`${this.cakeList[i].name}, menit ke ${j}: DONE! Let's eat cake!!!`);
+                } else if (j === this.cakeList[i].bakingTime - 5) {
+                    console.log(`${this.cakeList[i].name}, menit ke ${j}: almost done, almost!`);
+                } else if (j > this.cakeList[i].bakingTime) {
+                    console.log(`${this.cakeList[i].name}, menit ke ${j}: BURTNTTT!`);
+                } else {
+                    console.log(`${this.cakeList[i].name}, menit ke ${j}: not done yet, be patient.`);
+                }
+            }
+        }
+    }
+}
+
+let cake = new Cake();
+let choco = new ChocCake();
+let peanut = new PeanutCake();
+let cheese = new CheeseCake();
+
+let cakeList = [];
+cakeList.push(choco);
+cakeList.push(peanut);
+cakeList.push(cheese);
+
+let oven = new Oven(cakeList);
+
+console.log(oven);
+oven.bake(45);
