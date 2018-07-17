d971b3cea55003b7bf865ed9f4934e3f635ad4a8
joanlamrack <joanlamrack@gmail.com>
2018-07-17 10:31:31 +0700

    added code and 2 extra class script

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -1,10 +1,19 @@
-// Answer These Questions:
-//
-// - What are essential classes?
-// - What attributes will each class have?
-// - What interface will each class provide?
-// - How will the classes interact with each other?
-// - Which classes will inherit from others, if any?
-//
-//
-// Your code here
+const Cookie = require("./cookies.js");
+const Oven = require("./oven.js");
+
+let chocoCookie = new Cookie("cokelat",20);
+let peanutCookie = new Cookie("kacang",30);
+let cheeseCookie = new Cookie("keju",35);
+
+Oven.addCookie(chocoCookie);
+Oven.addCookie(peanutCookie);
+Oven.addCookie(cheeseCookie);
+
+for(let minutes = 0 ; minutes<=35 ; minutes+=5){
+	console.log(`Menit ke ${minutes} :`);
+	for(let cookie of Oven.cookiesInside()){
+		if(minutes!== 0)cookie.bake(5);
+		console.log(`\t Kue ${cookie.nama} : ${cookie.cookieStatus}`);
+	}
+}
+
