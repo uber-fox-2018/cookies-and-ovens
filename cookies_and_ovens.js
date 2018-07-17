2ff02315a14912bb36943a3737d583cdcfcdc1cf
vinesto <oqvinesto@gmail.com>
2018-07-17 23:48:36 +0700

    done

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -8,3 +8,57 @@
 //
 //
 // Your code here
+
+class Kue{
+    constructor(name,time){
+        this.name = name
+        this.time = time
+    }
+}
+
+class KueKacang extends Kue{
+    constructor(name,time){
+        super(name,time)
+    }
+}
+
+class KueCoklat extends Kue{
+    constructor(name,time){
+        super(name,time)
+    }
+}
+
+class KueKeju extends Kue{
+    constructor(name,time){
+        super(name,time)
+    }
+}
+
+class Oven{
+    constructor(cook){
+        this.cook = cook
+        this.loyang = []
+    }
+
+    bake(time){
+        let startTime = 5
+        for(let i = 0; i<time; i+=5){
+            if(i<this.cook.time-5){
+                console.log(`${this.cook.name}, menit ke ${i}: mentah`)
+            }else if(i === this.cook.time-5){
+                console.log(`${this.cook.name}, menit ke ${i}: hampir`)
+            }else if(i === this.cook.time){
+                console.log(`${this.cook.name}, menit ke ${i}: matang`)
+            }else{
+                console.log(`${this.cook.name}, menit ke ${i}: hangus`)
+            }
+
+        }
+
+    }
+}
+
+let chocolateCake = new KueCoklat('KueCoklat',20)
+let cook = new Oven (chocolateCake)
+
+cook.bake(30)
