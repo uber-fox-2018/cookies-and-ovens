873b5028464c7aaf8d82559923e7b294894e7a30
minzarddillah <minzard.dillah@gmail.com>
2018-07-18 09:03:29 +0700

    extend ke cake.js

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -1,10 +1,47 @@
-// Answer These Questions:
-//
-// - What are essential classes?
-// - What attributes will each class have?
-// - What interface will each class provide?
-// - How will the classes interact with each other?
-// - Which classes will inherit from others, if any?
-//
-//
-// Your code here
+const Cake = require('./cake.js')
+class PeanutButter extends Cake{
+    constructor(nama,lamaMasak){
+        super('Peanut Butter', 20)
+    }
+}
+class ChocolateChip extends Cake{
+    constructor(nama,lamaMasak){
+        super('Chocolate Chip', 15)
+    }
+}
+class ChocolateCheese extends Cake{
+    constructor(nama,lamaMasak){
+        super('Chocolate Cheese', 15)
+    }
+
+}
+class Oven {
+    constructor(){
+        this.listKue = []
+    }
+    bake(){
+        for(let i = 0 ; i < this.listKue.length ; i++){
+            for(let j = 5 ; j < this.listKue[i].lamaMasak+10 ; j+=5){
+                if(j === this.listKue[i].lamaMasak){
+                    console.log(this.listKue[i].name + ' menit ke '+j+' : Matang')
+                }else if(j <= this.listKue[i].lamaMasak-10){
+                    console.log(this.listKue[i].name + ' menit ke '+j+' : Mentah')
+                }else if(j <= this.listKue[i].lamaMasak-5){
+                    console.log(this.listKue[i].name + ' menit ke '+j+' : Hampir matang')
+                }else{
+                    console.log(this.listKue[i].name + ' menit ke '+j+' : Hangus')
+                }
+            }
+        }
+    }
+}
+
+var cake = new Cake
+var peanut = new PeanutButter
+var chocolateChip = new ChocolateChip
+var chocolateCheese = new ChocolateCheese
+var oven = new Oven
+oven.listKue.push(peanut)
+oven.listKue.push(chocolateChip)
+oven.listKue.push(chocolateCheese)
+console.log(oven.bake())
