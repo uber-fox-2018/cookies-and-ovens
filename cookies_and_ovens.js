f9cff0ecd12aacee72addd6d0385184a6349368c
gandryeanb <gstandrianb@gmail.com>
2018-07-19 14:54:35 +0700

    adding js file

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -1,10 +1,49 @@
-// Answer These Questions:
-//
-// - What are essential classes?
-// - What attributes will each class have?
-// - What interface will each class provide?
-// - How will the classes interact with each other?
-// - Which classes will inherit from others, if any?
-//
-//
-// Your code here
+class Cookie {
+    constructor(name, longBaked, bakedStatus) {
+        this.name = name
+        this.longBaked = longBaked
+        this.bakedStatus = bakedStatus
+    }
+    bakedStatusOnOven(num) { 
+        if (num >= this.longBaked + 5) {
+            this.bakedStatus = `hangus`
+        } else if (num === this.longBaked) {
+            this.bakedStatus = `matang`
+        } else if (num <= this.longBaked - 10) {
+            this.bakedStatus = `mentah`
+        } else if (num <= this.longBaked - 5) {
+            this.bakedStatus = `hampir matang`
+        } 
+    }
+}
+
+class Oven {
+    constructor() {
+        this.cakes = []
+        this.timer = 0
+    }
+    putCookie(obj,amount) {
+        for (let i = 0; i < amount; i++) {
+            this.cakes.push(obj)
+        }
+    }
+    bakeCookie(times) {
+        for (let j = 0; j < times; j++) {
+            this.timer += 5
+            for (let i = 0; i < this.cakes.length; i++) {
+                this.cakes[i].bakedStatusOnOven(this.timer)
+                console.log(`Kue ${this.cakes[i].name}, menit ke ${this.timer} : ${this.cakes[i].bakedStatus}`)    
+            }
+        }
+    }
+}
+
+const peanutCookie = new Cookie(`peanut cookie`, 30, `mentah`)
+const chocolateCookie = new Cookie(`chocolate cookie`, 20, `mentah`)
+const cheseeCookie = new Cookie(`chesee cookie`, 35, `mentah`)
+
+const oven = new Oven()
+
+oven.putCookie(peanutCookie,1)
+console.log(oven.cakes)
+oven.bakeCookie(6)
